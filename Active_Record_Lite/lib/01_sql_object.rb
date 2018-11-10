b96e606409cb1aa48785d545d8de91102dd3fb36
Jonnatan Ortiz <ortizjs@uci.edu>
2018-11-09 18:09:23 -0800

    Work on CSS exercise and Actie Record lite

---
+++ Active_Record_Lite/lib/01_sql_object.rb
@@ -0,0 +1,85 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
+  end
+
+  def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |val|
+        self.attributes[name] = val
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @@table_name = table_name
+
+  end
+
+  def self.table_name
+    self.to_s.downcase + 's'
+
+  end
+
+  def self.all
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(results)
+  end
+
+  def self.parse_all(results)
+    results.map {|result| self.new(result)}
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
